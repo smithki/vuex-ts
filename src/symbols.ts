590d5c22ec66274b42b5a5b10caa6b55810f9b1a
Ian K Smith <smithki707@gmail.com>
2019-01-11 10:38:03 -0700

    Implement strategy that retains "find all references" feature in VS Code

--- src/symbols.ts
+++ src/symbols.ts
@@ -1,3 +1,9 @@
+// Exposed
 export const state = Symbol('VuexTs state getter');
 export const context = Symbol('VuexTs context getter');
 export const rootState = Symbol('VuexTs rootState getter');
+
+// Internal
+export const staticGetters = Symbol('VuexTs static getters');
+export const staticMutations = Symbol('VuexTs static mutations');
+export const staticActions = Symbol('VuexTs static actions');
--- src/types.ts
+++ src/types.ts
@@ -16,14 +16,10 @@ export type ReturnTypeOrPlainProperty<T> = T extends (...args: any[]) => any ? R
 // --- Getters -------------------------------------------------------------- //
 
 export type StaticGetters = { [key: string]: (vuexState: any, vuexRootState: any) => any };
-export type MappedGetters<T extends ModuleGetters<any, any>> = {
-  [P in Exclude<KnownKeys<T>, typeof state | typeof rootState>]: T[P]
-};
 
 // --- Mutations ------------------------------------------------------------ //
 
 export type StaticMutations = { [key: string]: (vuexState: any, payload: any) => void };
-export type MappedMutations<T extends ModuleMutations<any>> = { [P in KnownKeys<T>]: T[P] };
 export type CommitFunc<T extends ModuleMutations<any>> = (
   mutationName: Exclude<KnownKeys<T>, typeof state>,
   ...payload: ArgumentTypes<T[typeof mutationName]>
@@ -32,7 +28,6 @@ export type CommitFunc<T extends ModuleMutations<any>> = (
 // --- Actions -------------------------------------------------------------- //
 
 export type StaticActions = { [key: string]: (vuexContext: ActionContext<any, any>, payload: any) => Promise<any> };
-export type MappedActions<T extends ModuleActions<any, any>> = { [P in KnownKeys<T>]: T[P] };
 
 export type DispatchFunc<T extends ModuleActions<any, any>> = (
   actionName: Exclude<KnownKeys<T>, typeof context | typeof state | typeof rootState>,

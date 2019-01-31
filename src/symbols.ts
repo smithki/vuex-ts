96bc6703baab81484b307ff9409133487670e742
Ian K Smith <smithki707@gmail.com>
2019-01-30 21:55:58 -0700

    More descriptive comments

--- src/symbols.ts
+++ src/symbols.ts
@@ -1,9 +1,11 @@
-// Exposed
+// --- Exposed --- //
 export const state = Symbol('VuexTs state getter');
 export const context = Symbol('VuexTs context getter');
 export const rootState = Symbol('VuexTs rootState getter');
 
-// Internal
+// --- Internal --- //
+// Using symbols for internal-only properties leads to cleaner editor
+// integration, showing only relevant methods and properties to the end-user.
 export const staticGetters = Symbol('VuexTs static getters');
 export const staticMutations = Symbol('VuexTs static mutations');
 export const staticActions = Symbol('VuexTs static actions');
